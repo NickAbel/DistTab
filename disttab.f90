c3b7d320d5f3c04b952668613c68b9c9dc9923cf
Nicholas Abel <nabel@bsc.es>
2021-05-17 17:44:16 +0200

    Get blocks from other ranks. Index lookup table globally (ranks > 0's tables don't begin at 1)

--- disttab.f90
+++ disttab.f90
@@ -10,7 +10,8 @@ program get_ex
   integer :: x(2), y ! Coordinate index (x(1),x(2)), flat index y
   integer :: block_bounds(4)
   integer :: i, j, k
-  double precision :: lookup_table(n(1)*n(2),n(3)), values_fetched(q(1)), z
+  double precision :: value_fetched, z
+  double precision, dimension(:,:), allocatable :: lookup_table
   integer :: window
   integer :: integer_size, dbl_size
   integer(kind=mpi_address_kind) :: win_size, lookup_table_size
@@ -23,13 +24,15 @@ program get_ex
   call mpi_comm_rank(mpi_comm_world, rank, ierror)
   call mpi_comm_size(mpi_comm_world, nprocs, ierror)
 
+  ! Allocate lookup table
+  allocate(lookup_table(((n(1)*n(2)*rank) + 1):(n(1)*n(2)*(rank+1)), n(3)))
+
   ! Open lookup table file
   lookup_table_filename = 'tables/2d.dat'
   open(unit = 46, file = lookup_table_filename, status='old', action='read')
 
-  ! Compute size of lookup table and window block size
+  ! Compute size of lookup table in bytes for window creation
   lookup_table_size = n(1)*n(2)*n(3)*dbl_size
-  win_size = q(1)*dbl_size
 
   ! Insist on some comm size
   if (nprocs .ne. 2) then
@@ -43,27 +46,26 @@ program get_ex
   enddo
 
   ! Populate lookup table with self-explanatory entries
-  do i = 1,n(3)
-    do j = 1,n(2)
-      do k = 1,n(1)
-        y = coords2flat((/ k,j /))
-        x = flat2coords(y)
-        !!! Debug output for indexing conversion
-        !if (rank .eq. 1) then
-        !  print *, k-x(1), j-x(2), n(1)*n(2)*rank+y
-        !endif 
-        lookup_table(y,i) = y + rank*n(1)*n(2)
-      enddo
+  do j = 1,n(3)
+    do k = lbound(lookup_table,dim=1), ubound(lookup_table,dim=1)
+      !y = coords2flat((/ k,j /))
+      !x = flat2coords(y)
+      !!! Debug output for indexing conversion
+      !if (rank .eq. 1) then
+      !  print *, k-x(1), j-x(2), n(1)*n(2)*rank+y
+      !endif 
+      lookup_table(k,j) = k + 0.d0
     enddo
   enddo
+  !print *, lookup_table
 
   ! Create memory window covering whole sub-table on each rank
-  call mpi_win_create(lookup_table, q(1)*win_size, dbl_size, mpi_info_null, mpi_comm_world, window, ierror)
+  call mpi_win_create(lookup_table, lookup_table_size, dbl_size, mpi_info_null, mpi_comm_world, window, ierror)
   call mpi_win_fence(0, window, ierror)
 
-  ! If block is available on own rank... print it, for now
+  ! Pick a random number in the global lookup table range
   call random_number(z)
-  y = ceiling(z*n(1)*n(2))
+  y = ceiling(z*n(1)*n(2)*nprocs)
   x = flat2coords(y)
 
   ! Print information about the point requested x
@@ -83,33 +85,28 @@ program get_ex
   ! Dynamically allocate one partition
   allocate(partition(block_bounds(1):block_bounds(2), block_bounds(3):block_bounds(4)))
 
+  ! Determine if the partition entries are on-table. MPI_GET any entries that are not.
   do i = block_bounds(1), block_bounds(2)
     do j = block_bounds(3), block_bounds(4)
       y = coords2flat((/ i,j /))
-      partition(i,j) = merge(y, -1, find_block_rank_flat(y) .eq. rank)
+      partition(i,j) = merge(lookup_table(y,1), -1.0d0, find_block_rank_flat(y) .eq. rank)
       print *, i,j, " corresponds to x = ", y, merge("     (On table at rank ", " (OUT OF TABLE ON RANK ", &
         find_block_rank_flat(y) .eq. rank), rank, ") "
+      if (partition(i,j) .eq. -1.0) then
+        print *, "MPI GETting entry ", y, " from table ", find_block_rank_flat(y)
+        target_displacement = mod(y-1,250)
+        call mpi_get(value_fetched, 1, mpi_double, find_block_rank_flat(y), &
+          target_displacement, 1, mpi_double, window, ierror)
+        partition(i,j) = value_fetched
+      endif
     enddo
   enddo
   print *, partition
 
-  ! Deallocate here for now while testing
-  deallocate(partition)
-
-  ! If block is available on other rank, get it and... print it, for now
-  if (rank .eq. 0) then
-    target_displacement = 0
-    call mpi_get(values_fetched, q(1), mpi_double, 1, target_displacement, q(1), mpi_double, window, ierror)
-  else if (rank .eq. 1) then
-    target_displacement = 0
-    call mpi_get(values_fetched, q(1), mpi_double, 0, target_displacement, q(1), mpi_double, window, ierror)
-  endif
-  call mpi_win_fence(0, window, ierror)
-
-  ! Print, clean up, exit
-  !print *, "Fetched value = ", values_fetched, rank
-  call mpi_win_free(window, ierror)
+  ! Clean up, exit
   close(46)
+  deallocate(partition)
+  call mpi_win_free(window, ierror)
   call mpi_finalize(ierror)
 
 contains
@@ -120,19 +117,23 @@ contains
 
   !!!!
   ! Return upper and lower bounds of rank in global flat indices
+  ! Assuming the tables on each rank are NOT indexed starting at 1
   function find_rank_bounds(bloc) result(bounds)
     integer, intent(in)  :: bloc
     integer, dimension(2) :: bounds
-    bounds(1) = lbound(lookup_table,dim=1)+n(1)*n(2)*rank
-    bounds(2) = ubound(lookup_table,dim=1)+n(1)*n(2)*rank
+    bounds(1) = lbound(lookup_table,dim=1)!+n(1)*n(2)*rank
+    bounds(2) = ubound(lookup_table,dim=1)!+n(1)*n(2)*rank
   end function
 
   !!!!
   ! Return rank where a flat index f is located
+  ! TODO Using the max() function is a hack-y fix for the problem
+  ! ie when n1*n2 = 250, 500/250 = 2 but entry 500 is on table 1.
+  ! This might not work in general cases
   function find_block_rank_flat(f) result(bloc)
     integer, intent(in)  :: f
     integer :: bloc
-    bloc = f/(n(1)*n(2))
+    bloc = max(0,(f-1)/(n(1)*n(2)))
   end function
 
   ! Return rank where a coordinate index (c(1),c(2)) is located
@@ -149,14 +150,14 @@ contains
     partition_index(1) = ceiling(real(c(1))/real(q(1)))
     partition_index(2) = ceiling(real(c(2))/real(q(2)))
   end function
-  
+
   ! Find a global coordinate (c(1),c(2))'s local (on partition) coordinates
   function find_local_partition_entry(c) result(local_entry)
-     integer, dimension(2) :: c
+    integer, dimension(2) :: c
     integer, dimension(2) :: local_entry
     local_entry(1) = merge(mod(c(1),q(1)),q(1),mod(c(1),q(1)) .ne. 0)
     local_entry(2) = merge(mod(c(2),q(2)),q(2),mod(c(2),q(2)) .ne. 0)
-    end function
+  end function
 
 
   ! Return coordinate bounds of flat index f's partition
